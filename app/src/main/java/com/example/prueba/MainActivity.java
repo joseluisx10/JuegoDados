533cc5c0a600109dfee1d7e8f69d090f7433b960
joseluisx10 <joseluis.quispe_91@hotmail.com>
2023-11-07 03:00:16 -0300

    diseño de una de las vistas del juego

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -9,6 +9,7 @@ import android.service.autofill.FieldClassification;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -24,37 +25,37 @@ public class MainActivity extends AppCompatActivity {
         ImageView dado3 = findViewById(R.id.img3);
         ImageView dado4 = findViewById(R.id.img4);
         ImageView dado5 = findViewById(R.id.img5);
-        ImageView dado6 = findViewById(R.id.img6);
+
         pulsar.setOnClickListener(new View.OnClickListener() {
 
-            private Integer[] imagenesID = new Integer[]{R.drawable.dado1, R.drawable.dado2, R.drawable.dado3, R.drawable.dado4, R.drawable.dado5, R.drawable.dado6};
+            private TextView txtUserName = findViewById(R.id.textView2);
+            private Integer[] imagenesID = new Integer[]{R.drawable.dado1, R.drawable.dado2, R.drawable.dado3, R.drawable.dado4, R.drawable.dado5};
             @Override
             public void onClick(View view) {
 
+                txtUserName.setText("User name: Matias");
+
                 Random r = new Random();
 
-
-                /*int valorDado1 = r.nextInt(6)+1;
-                int valorDado2 = r.nextInt(6)+1;
-                int valorDado3 = r.nextInt(6)+1;
-                int valorDado4 = r.nextInt(6)+1;
-                int valorDado5 = r.nextInt(6)+1;
-                int valorDado6 = r.nextInt(6)+1;*/
                 int valorDado1 = imagenesID[r.nextInt(imagenesID.length)];
                 int valorDado2 = imagenesID[r.nextInt(imagenesID.length)];
                 int valorDado3 = imagenesID[r.nextInt(imagenesID.length)];
                 int valorDado4 = imagenesID[r.nextInt(imagenesID.length)];
                 int valorDado5 = imagenesID[r.nextInt(imagenesID.length)];
-                int valorDado6 = imagenesID[r.nextInt(imagenesID.length)];
+
 
                 dado1.setImageResource(valorDado1);
                 dado2.setImageResource(valorDado2);
                 dado3.setImageResource(valorDado3);
                 dado4.setImageResource(valorDado4);
                 dado5.setImageResource(valorDado5);
-                dado6.setImageResource(valorDado6);
+
+
+
 
             }
+
+
         });
     }
 }
