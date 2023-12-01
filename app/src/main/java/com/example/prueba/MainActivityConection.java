5a948826f1ac5f24e19b60b076afb9e63c8cae06
joseluisx10 <joseluis.quispe_91@hotmail.com>
2023-12-01 14:15:35 -0300

    feat:add desarrollo del mock pdf

--- app/src/main/java/com/example/prueba/MainActivityConection.java
+++ app/src/main/java/com/example/prueba/MainActivityConection.java
@@ -2,10 +2,12 @@ package com.example.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.sql.Connection;
 
@@ -16,6 +18,11 @@ public class MainActivityConection extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_conection);
         checkConnection();
+    }
+
+    public void seguirJugandoOnClick(View v){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 
     public void checkConnection(){
