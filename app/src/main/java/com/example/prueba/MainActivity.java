f4579ec9c39226856d6094aa7ed5d76d7a15db21
Jose Luis Quispe <joseluis.9108@hotmail.com>
2024-07-24 02:07:57 -0300

    fix: add firebasestore metodo getInstance

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-
+        db = FirebaseFirestore.getInstance();
         ImageView dado1;
         ImageView dado2;
         ImageView dado3;
